af5cb3273541de8400eb5f2dd9e180ade191415c
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-30 17:01:55 -0300

    Funções Cadastrar (com verificação de CPF) e Listar

--- src/controllers/PessoaController.ts
+++ src/controllers/PessoaController.ts
@@ -3,7 +3,7 @@ import PessoaSchema from "../models/PessoaSchema";
 
 class PessoaController{
     async listar(request: Request, response: Response) {
-        response.status(200).json({msg: "Rota da listagem" });
+        response.status(200).json(await PessoaSchema.find({}));
     }
 
     async buscarPorCpf(request: Request, response: Response) {
@@ -11,7 +11,22 @@ class PessoaController{
     }
 
     async cadastrar(request: Request, response: Response) {
-        response.status(200).json({msg: "Rota do cadastro" });
+        try {
+            const { cpf } = request.body;
+            if(await PessoaSchema.exists({ cpf })){ // await para esperar a verificação
+                response.status(409).json({ msg: "Essa pessoa já existe!" }); // 409 = cód. conflito
+            }else {
+                const pessoa = request.body; // o corpo da requisição envia o objeto para a constante pessoa
+                const novaPessoa = await PessoaSchema.create(pessoa);
+                response.status(201).json(novaPessoa);
+            }
+        } catch (error) {
+            response.status(400).json({
+                objeto: error,
+                msg: "Falha na validação",
+                erro: true
+            })
+        }
     }
 
     async alterar(request: Request, response: Response) {
