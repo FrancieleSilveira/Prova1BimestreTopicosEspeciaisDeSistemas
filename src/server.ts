f03d6b2d0f3b0e7fda4367349ca44132af79052f
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-30 13:58:23 -0300

    conexão com banco de dados

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,11 @@
-console.clear();
-console.log("Testando a configuração de ambiente");
+console.clear(); // limpa o console
+
+import express from "express"; // importa express da biblioteca express
+import { mongoose } from "./config/database"; // importa mongoose do arquivo informado
+
+const app = express(); // cria a aplicação apartir do express e atribui a constante app
+const db = mongoose; // atribui o mongoose a constante db
+
+app.listen(1234, () => {   // roda a aplicação na porta definida, no caso 1234
+    console.log("O servidor está rodando...");
+});
